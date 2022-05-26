99dbebe7bcfd01f90bf7911fd13c6c3e8faec449
Nicolas <nicolasvalbuenaro@gmail.com>
2022-05-25 22:32:12 -0500

    fix  commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { SignInModule } from './pages/sign-in/sign-in.module';
 import { AppComponent } from './app.component';
-import { UsariosComponent } from './pages/usarios/usarios.component';
-import { RolesComponent } from './pages/roles/roles.component';
 import { HomeComponent } from './pages/Home/home.component';
 
 
@@ -13,8 +11,6 @@ import { HomeComponent } from './pages/Home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
-    UsariosComponent,
-    RolesComponent,
     HomeComponent
   ],
   imports: [
